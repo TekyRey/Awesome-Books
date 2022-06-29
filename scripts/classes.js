1a78c093a537787c51f9242378ea290096d8cc00
henry <88551100+henrykgggg@users.noreply.github.com>
2022-06-29 16:19:29 -0700

    solved linters

--- scripts/classes.js
+++ scripts/classes.js
@@ -8,30 +8,30 @@
 // class UI {
 //     static displayBooks() {
 //       const books = Store.getBooks();
-  
+
 //       books.forEach((book) => UI.addBookToList(book));
 //     }
-  
+
 //     static addBookToList(book) {
 //       const list = document.querySelector('#book-list');
-  
+
 //       const row = document.createElement('tr');
-  
+
 //       row.innerHTML = `
 //         <td>${book.title}</td>
 //         <td>${book.author}</td>
 //         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
 //       `;
-  
+
 //       list.appendChild(row);
 //     }
-  
+
 //     static deleteBook(el) {
 //       if(el.classList.contains('delete')) {
 //         el.parentElement.parentElement.remove();
 //       }
 //     }
-  
+
 //     static showAlert(message, className) {
 //       const div = document.createElement('div');
 //       div.className = `alert alert-${className}`;
@@ -39,15 +39,14 @@
 //       const container = document.querySelector('.container');
 //       const form = document.querySelector('#book-form');
 //       container.insertBefore(div, form);
-  
+
 //     }
-  
+
 //     static clearFields() {
 //       document.querySelector ('#title').value ='',
 //       document.querySelector('#author').value = '';
 //     }
 //   }
-  
 
 //   class Store {
 //     static getBooks() {
@@ -57,39 +56,37 @@
 //       } else {
 //         books = JSON.parse(localStorage.getItem('books'));
 //       }
-  
+
 //       return books;
 //     }
-  
+
 //     static addBook(book) {
 //       const books = Store.getBooks();
 //       books.push(book);
 //       localStorage.setItem('books', JSON.stringify(books));
 //     }
-  
+
 //     static removeBook(title) {
 //       const books = Store.getBooks();
-  
+
 //       books.forEach((book, index) => {
 //         if(book.title === title) {
 //           books.splice(index, 1);
 //         }
 //       });
-  
+
 //       localStorage.setItem('books', JSON.stringify(books));
 //     }
 //   }
 
 //   document.addEventListener('DOMContentLoaded', UI.displayBooks);
-  
+
 //   document.querySelector('#book-form').addEventListener('submit', (e) => {
 //     e.preventDefault();
-  
 
 //     const title = document.querySelector('#title').value;
 //     const author = document.querySelector('#author').value;
-    
-  
+
 //     if(title === '' || author === '' ) {
 //       UI.showAlert('Please fill in all fields');
 //     } else {
@@ -102,9 +99,9 @@
 //       UI.clearFields();
 //     }
 //   });
-  
+
 //   document.querySelector('#book-list').addEventListener('click', (e) => {
 //     UI.deleteBook(e.target);
-  
+
 //     Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 //   });
